b4d53207053819fa239906c00dc66fe4febb42c3
ArkejGit <robert.krawczyk92@gmail.com>
2018-03-26 18:10:11 +0200

    Create simple navbar with header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">Star Viever</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+      </Navbar>;
+
         <p>Star Viewer</p>
       </div>
     );

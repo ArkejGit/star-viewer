78629fc170af0eb005eae825c1cffd59dd4bc8dc
ArkejGit <robert.krawczyk92@gmail.com>
2018-03-26 20:21:05 +0200

    Fetch data from API

--- src/App.js
+++ src/App.js
@@ -2,14 +2,39 @@ import React, { Component } from 'react';
 import AppNavbar from './AppNavbar/AppNavbar';
 import './App.css';
 
+const API = 'http://www.astropical.space/astrodb/api.php?';
+
 class App extends Component {
+
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      stars: []
+    }
+
+    this.getStars( 1 );
+  }
+
+  getStars( magnitude ) {
+    fetch(`${API}table=stars&which=magnitude&limit=${magnitude}&format=json`)
+      .then( res => res.json() )
+      .catch( err => console.log( err ) )
+      .then( data => {
+        this.setState({
+          stars: data.hipstars
+        })
+        console.log( this.state.stars );
+      });
+  }
+
   render() {
     return (
       <div className="App">
 
         <AppNavbar />
 
-        <div className="container text-center">
+        <div className="container">
           <div className="sphere"></div>
         </div>
 

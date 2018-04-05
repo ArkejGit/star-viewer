4489666c1dee40d6a43260302db6cc70be062f8d
ArkejGit <robert.krawczyk92@gmail.com>
2018-04-05 19:21:26 +0200

    Add tooltip to the 'Magnitude' text

--- src/InputRangeContainer/InputRangeContainer.js
+++ src/InputRangeContainer/InputRangeContainer.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import InputRange from 'react-input-range';
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 import 'react-input-range/lib/css/index.css';
-import './InputRangeContainer.css'
+import './InputRangeContainer.css';
+
+const tooltip = (
+  <Tooltip id="tooltip">
+    <p>In astronomy, magnitude is a logarithmic measure of the brightness of an object.</p>
+    <p>The brighter an object appears, the lower the value of its magnitude.</p>
+  </Tooltip>
+);
 
 const InputRangeContainer = ({ magnitudeLimit, onChange }) => {
   return(
     <div>
-      <p>Magnitude</p>
-          <div className="input-range-container">
-            <span className="glyphicon glyphicon-star glyphicon-big"></span>
-            <InputRange
-              maxValue={5}
-              minValue={0}
-              value={ magnitudeLimit }
-              onChange={ value => onChange(value) }
-            />
-            <span className="glyphicon glyphicon-star"></span>
-          </div>
+
+      <OverlayTrigger placement="bottom" overlay={tooltip}>
+        <p>Magnitude</p>
+      </OverlayTrigger>
+
+        <div className="input-range-container">
+          <span className="glyphicon glyphicon-star glyphicon-big"></span>
+          <InputRange
+            maxValue={5}
+            minValue={0}
+            value={ magnitudeLimit }
+            onChange={ value => onChange(value) }
+          />
+          <span className="glyphicon glyphicon-star"></span>
+        </div>
+
     </div>
   )
 };

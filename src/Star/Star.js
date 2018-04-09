aff46e9b2352cb1717fd7061b3925db4d8fc16dd
ArkejGit <robert.krawczyk92@gmail.com>
2018-04-09 19:00:01 +0200

    Add tooltip to every single star

--- src/Star/Star.js
+++ src/Star/Star.js
@@ -1,38 +1,48 @@
 import React from 'react';
 import './Star.css';
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
+
+/**
+ * calculatePositionOfStar(ra,de)
+ *
+ * Takes two basic astronomical coordinates of a star (right ascension & declination) and returns distances from top and left of Sphere container (expressed as a percentage of entire container's width).
+ *
+ * @param {Number}   ra     Right ascension
+ * @param {Number}   de     Declination
+ * 
+ * @return {Object}     Object with two properties: {String} top, {String} left
+ */
+const calculatePositionOfStar = (ra, de) => {
+
+	// calculate top
+	const top = `${50 - 0.555 * de}%`;
+
+	// calculate left
+	const h = 1 - 0.011 * Math.abs(de);
+	const chordWidth = 2 * Math.sqrt( 2 * h - h * h );
+	const raAsPercentage = ra * 8.333;
+	const left = `${ (100 - chordWidth * 50) / 2 + chordWidth * 0.5 * raAsPercentage }%`;
+
+	return { top: top, left: left };
+};
+
 
 const Star = ({ name, ra, de }) => {
 
-	/**
-	 * calculatePositionOfStar(ra,de)
-	 *
-	 * Takes two basic astronomical coordinates of a star (right ascension & declination) and returns distances from top and left of Sphere container (expressed as a percentage of entire container's width).
-	 *
-	 * @param {Number}   ra     Right ascension
-	 * @param {Number}   de     Declination
-	 * 
-	 * @return {Object}     Object with two properties: {String} top, {String} left
-	 */
-	const calculatePositionOfStar = (ra, de) => {
-
-		// calculate top
-		const top = `${50 - 0.555 * de}%`;
-
-		// calculate left
-		const h = 1 - 0.011 * Math.abs(de);
-		const chordWidth = 2 * Math.sqrt( 2 * h - h * h );
-		const raAsPercentage = ra * 8.333;
-		const left = `${ (100 - chordWidth * 50) / 2 + chordWidth * 0.5 * raAsPercentage }%`;
-
-		return { top: top, left: left };
-	};
-
 	const { top, left } = calculatePositionOfStar( ra, de );
+
+	const tooltip = (
+	  <Tooltip id="tooltip">
+	    <p>{ name ? name : 'unknown name' }</p>
+	  </Tooltip>
+	);
 	
 	return(
-		<div className='star'
-			style={{ top: top, left: left, display: `${ ra > 12 ? 'none' : 'block' }` }}>
-		</div>
+		<OverlayTrigger placement="top" overlay={tooltip}>        
+			<div className='star'
+				style={{ top: top, left: left, display: `${ ra > 12 ? 'none' : 'block' }` }}>
+			</div>
+ 		</OverlayTrigger>
 	);
 
 };

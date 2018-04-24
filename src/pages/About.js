51165e7a54cd852a31363436e25087a0bb25cc32
ArkejGit <robert.krawczyk92@gmail.com>
2018-04-24 19:18:10 +0200

    Add app's description in About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const About = () => {
-  return(
-    <div>
-      <p>This is the About page.</p>
-    </div>
-  )
+	return(
+		<div>
+			<h2>About</h2>
+			<p>This is a React app that displays position of stars on the celestial sphere.</p>
+			<p>The app uses <a href="http://www.astropical.space/astrodb/apiref.php">astropical</a> API.</p>
+			<br/>
+			<p><i>Created by <a href="https://github.com/ArkejGit">ARKEJ</a>.</i></p>
+		</div>
+	)
 };
 
 export default About;

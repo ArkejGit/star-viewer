a29378226f82138aced83b9f380e0a62183c87bb
ArkejGit <robert.krawczyk92@gmail.com>
2018-05-01 17:36:17 +0200

    Enable express to serve index.html when doesn't recognize the route

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(‘/’, express.static(`${__dirname}/client/build`));
 };
 
+// express will serve up index.html if it doesn't recognize the route
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
 app.get('/api/stars', (req, res) => {
 
 	request(`${API}table=stars&which=magnitude&limit=4.97&format=json`, (err, res, body) => {
